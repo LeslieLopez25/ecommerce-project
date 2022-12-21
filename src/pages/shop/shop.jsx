c24c3111d0e05353cbc66f2180365c862cb2cdb3
Leslie Lopez <leslie2515lopez@gmail.com>
2022-12-20 23:47:24 -0600

    Created shop component

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { PRODUCTS } from "../../products";
 
 export const Shop = () => {
-  return <div>shop</div>;
+  return (
+    <div className="shop">
+      <div className="shopTitle">
+        <h1> Tech Stop </h1>
+      </div>
+      <div className="products">
+        {PRODUCTS.map((product) => (
+          <Product />
+        ))}
+      </div>
+    </div>
+  );
 };

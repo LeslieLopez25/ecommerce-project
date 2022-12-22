4a2189a62c671c977baca952aafbbad3beba26cb
Leslie Lopez <leslie2515lopez@gmail.com>
2022-12-21 22:37:56 -0600

    Finished setting up product component and shop css

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -2,5 +2,15 @@ import React from "react";
 
 export const Product = (props) => {
   const { id, productName, price, productImage } = props.data;
-  return <div c></div>;
+  return (
+    <div className="product">
+      <img src={productImage} alt="product" />
+      <div className="description">
+        <p>
+          <b>{productName}</b>
+        </p>
+        <p> ${price}</p>
+      </div>
+    </div>
+  );
 };

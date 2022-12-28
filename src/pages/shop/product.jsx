c938e16c2678160cfd736d6877e899326b9b983e
Leslie Lopez <leslie2515lopez@gmail.com>
2022-12-27 23:23:08 -0600

    Added the shop-context to app.js and the increase of cart items to add to cart button in product component

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
 
 export const Product = (props) => {
   const { id, productName, price, productImage } = props.data;
+  const { addToCart, cartItems } = useContext(ShopContext);
+
+  const cartItemAmount = cartItems[id];
+
   return (
     <div className="product">
       <img src={productImage} alt="product" />
@@ -11,7 +16,9 @@ export const Product = (props) => {
         </p>
         <p> ${price}</p>
       </div>
-      <button className="addToCartBttn"> Add To Cart</button>
+      <button className="addToCartBttn" onClick={() => addToCart(id)}>
+        Add To Cart {cartItemAmount > 0 && <> ({cartItemAmount})</>}
+      </button>
     </div>
   );
 };

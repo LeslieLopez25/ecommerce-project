5fd1c9ccc2404174e74d9a40fe6767e09fa9484a
Leslie Lopez <leslie2515lopez@gmail.com>
2023-01-03 23:09:35 -0600

    Added navigate to shopping page on button in cart page

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -4,9 +4,13 @@ import { PRODUCTS } from "../../products";
 import { CartItem } from "./cart-item";
 import "./cart.css";
 
+import { useNavigate } from "react-router-dom";
+
 export const Cart = () => {
   const { cartItems, getTotalCartAmount } = useContext(ShopContext);
   const totalAmount = getTotalCartAmount();
+
+  const navigate = useNavigate();
 
   return (
     <div className="cart">
@@ -23,7 +27,7 @@ export const Cart = () => {
 
       <div className="checkout">
         <p> Subtotal: ${totalAmount}</p>
-        <button> Continue Shopping</button>
+        <button onClick={() => navigate("/")}> Continue Shopping</button>
         <button> Checkout </button>
       </div>
     </div>
